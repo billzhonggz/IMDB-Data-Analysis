a6ceeb26853a07854b2ffa5e812971d9c6e3e697
Bill ZHONG <billzhonggz@outlook.com>
2017-12-16 22:59:29 +0800

    Update director project: find out their works.

--- Project-Directors/Directors.py
+++ Project-Directors/Directors.py
@@ -10,11 +10,36 @@ import pandas as pd
 
 # Read csv.
 df = pd.read_csv('../Dataset/IMDB-Movie-Data.csv')
-# Extract columns
-df1 = df[['Director', 'Title', 'Revenue (Millions)']]
+
 # List all directors.
-directorList = df1.groupby('Director')['Revenue (Millions)'].agg(['sum', 'count'])
+directorList = df.groupby('Director')['Revenue (Millions)'].agg(['sum', 'count']).reset_index()
+# Delete invalid data
+directorList = directorList.dropna()
+# Get average revenue of each director.
+directorList['avg'] = directorList['sum'] / directorList['count']
 print(directorList)
 # Sorting by revenue, list the first five data.
-firstFive = directorList.sort_values('sum', ascending=False).head()
+firstFive = directorList.sort_values('avg', ascending=False).head()
 print(firstFive)
+# The first five directors are:
+#             Director      sum  count      avg
+# 261    James Cameron   760.51      1  760.510
+# 114  Colin Trevorrow   652.18      1  652.180
+# 341      Joss Whedon  1082.27      2  541.135
+# 377      Lee Unkrich   414.98      1  414.980
+# 208        Gary Ross   408.00      1  408.000
+
+# Searching their works.
+directorNames = firstFive['Director']
+for directorName in directorNames:
+    for index, row in df.iterrows():
+        if row['Director'] == directorName:
+            print(row['Director'] + ', ' + row['Genre'] + ', ' + row['Title'])
+# Result
+# James Cameron, Action,Adventure,Fantasy, Avatar
+# Colin Trevorrow, Action,Adventure,Sci-Fi, Jurassic World
+# Joss Whedon, Action,Sci-Fi, The Avengers
+# Joss Whedon, Action,Adventure,Sci-Fi, Avengers: Age of Ultron
+# Lee Unkrich, Animation,Adventure,Comedy, Toy Story 3
+# Gary Ross, Adventure,Sci-Fi,Thriller, The Hunger Games
+# Gary Ross, Action,Biography,Drama, Free State of Jones
