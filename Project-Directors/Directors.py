50de540290f8aad5f1e7b1733320a4d587444216
Bill ZHONG <billzhonggz@outlook.com>
2017-12-16 18:12:02 +0800

    Update project. Now can save first five directors sorted by revenue.

--- Project-Directors/Directors.py
+++ Project-Directors/Directors.py
@@ -3,7 +3,7 @@
 # 1. List all directors.
 # 2. Get the total revenues each directors got.
 # 3. Rank all directors by revenues got each film in average.
-# 4. List information of directed film(s) of the top-3 directors.
+# 4. List information of directed film(s) of the top-5 directors.
 # 5. Estimate the revenues of their next film by some algorithms.
 
 import pandas as pd
@@ -13,6 +13,8 @@ df = pd.read_csv('../Dataset/IMDB-Movie-Data.csv')
 # Extract columns
 df1 = df[['Director', 'Title', 'Revenue (Millions)']]
 # List all directors.
-directorList = df1.groupby(['Director'])
-directorFilmCount = directorList.size().reset_index(name='Film Count')
-directorTotalRevenue = directorList.sum()
+directorList = df1.groupby('Director')['Revenue (Millions)'].agg(['sum', 'count'])
+print(directorList)
+# Sorting by revenue, list the first five data.
+firstFive = directorList.sort_values('sum', ascending=False).head()
+print(firstFive)
